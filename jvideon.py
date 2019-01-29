477ff4ba5158684a421890c0eca21310876aa6d3
Edoardo Carra <carra@di.uniroma1.it>
2019-01-29 13:57:00 +0100

    .

--- jvideon.py
+++ jvideon.py
@@ -5,6 +5,7 @@ sys.path.append('modules')
 import os
 import json
 import operations
+import ntpath
 
 json_file="video.json"
 work_directory="jvideon"
@@ -41,14 +42,36 @@ def valid(videos_json):
 
 	return is_valid
 
+def apply_transformations(path, transformations, background):
+
+	(dirname, filename) = os.path.split(path)
+	input_video = filename
+	for transformation in transformations:
+		operation = transformation["operation"]
+		parameters = transformation["parameters"]
+
+		print("prendo "+ input_video)
+		print("applico "+ operation)
+		print("salvo "+ operation+"_"+filename)
+		input_video=operation+"_"+filename
+
+	return background
+
 def build(video_json):
 	name=video_json["name"]
 	background=video_json["background"]
 	input_videos=video_json["input"]
 	transformations=video_json["transformations"]
 
-
+	for i in range(0,len(input_videos)):
+		video_path = input_videos[i]
+		video_transformations = transformations[i] 
+		background = apply_transformations(video_path,video_transformations,background)
 	
+	#for each input video and for each respective transformation, generate an intermediate video
+	#intermediate video is saved in a temporary folder, and the name terminates with the name of the transformation applied on it 
+	#if there are more that one video, they will compose into the first one once all transformations on them are finished
+
 	#building logic
 
 if not os.path.isfile(json_file): 
